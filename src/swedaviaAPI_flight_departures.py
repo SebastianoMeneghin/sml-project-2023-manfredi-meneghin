d83f516c0a307d4803b0bf817e064ceda3c552d8
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2024-01-06 21:56:22 +0100

    Refresh swedaviaAPI script

--- src/swedaviaAPI_flight_departures.py
+++ src/swedaviaAPI_flight_departures.py
@@ -8,7 +8,6 @@ from utils import get_data, get_month
 # responseJson2 = response2.json()
 # print(responseJson2)
 
-
 # Get the subscription key from Swedavia API and set up it as header for the requests
 subscription_key = 'a9042dc249f34e02b9d7512a1d85aa70'
 headers = {
@@ -18,50 +17,19 @@ headers = {
 }
 
 # Make the API request for Swedavia API
-response3 = requests.get("https://api.swedavia.se/flightinfo/v2/ARN/departures/2023-12-02", headers = headers)
-responseJson3 = response3.json()
-#print(responseJson3)
+response = requests.get("https://api.swedavia.se/flightinfo/v2/ARN/departures/2023-12-02", headers = headers)
+flights_swedavia = response.json()
+print(flights_swedavia)
 
 # Get the flights from the Json file, then flatten them to get nested-attributes and convert it to a DataFrame
-flight_str = responseJson3['flights']
+flight_str = flights_swedavia['flights']
 flight_df = pd.json_normalize(flight_str)
 
 # Select the wanted data from the DataFrame, creating another_df
-flight_df = flight_df.rename(columns={'departureTime.actualUtc': 'actualDepTime', 'departureTime.scheduledUtc': 'scheduledDepTime', 'locationAndStatus.terminal' : 'terminal', 'diIndicator' : 'flightAreaType', 'airlineOperator.icao' : 'airline', 'locationAndStatus.flightLegStatus' : 'flightStatus', 'flightLegIdentifier.callsign' : 'flightIdentifier', 'flightLegIdentifier.flightDepartureDateUtc' : 'departureDate', 'flightLegIdentifier.departureAirportIcao' : 'departureAirport', 'flightLegIdentifier.arrivalAirportIcao' : 'arrivalAirport'})
-another_df = flight_df[["flightIdentifier", "airline", "departureDate", "flightAreaType", "arrivalAirport", "departureAirport", "terminal", "flightStatus", "scheduledDepTime", "actualDepTime"]]
-
-# Get the month from the datatime (as future input for the ML algorithm) and the data, as key for the Database
-data_key_column = another_df['departureDate'].map(lambda x : get_data(x))
-month_column    = another_df['departureDate'].map(lambda x : get_month(x))
-
-dep_df = another_df
-dep_df['departureDate'] = data_key_column
-dep_df['month']         = month_column
-
-
-print(dep_df.head().T)
+#flight_df = flight_df.rename(columns={'departureTime.actualUtc': 'actualDepTime', 'departureTime.scheduledUtc': 'scheduledDepTime', 'locationAndStatus.terminal' : 'terminal', 'diIndicator' : 'flightAreaType', 'airlineOperator.icao' : 'airline', 'locationAndStatus.flightLegStatus' : 'flightStatus', 'flightLegIdentifier.callsign' : 'flightIdentifier', 'flightLegIdentifier.flightDepartureDateUtc' : 'departureDate', 'flightLegIdentifier.departureAirportIcao' : 'departureAirport', 'flightLegIdentifier.arrivalAirportIcao' : 'arrivalAirport'})
+#another_df = flight_df[["flightIdentifier", "airline", "departureDate", "flightAreaType", "arrivalAirport", "departureAirport", "terminal", "flightStatus", "scheduledDepTime", "actualDepTime"]]
 
 
 
-### For OAG API
-########### Python 3.2 #############
-import urllib.request, json
 
-try:
-    url = "https://api.oag.com/flight-instances/?DepartureDateTime=2022-12-04&DepartureAirport=ESSA&Content=Status&CodeType=ICAO&Limit=100&version=v2"
 
-    hdr ={
-    # Request headers
-    'Cache-Control': 'no-cache',
-    'Subscription-Key': '1ba6f1f8d11143b698bb576269232c52',
-    }
-
-    req = urllib.request.Request(url, headers=hdr)
-
-    req.get_method = lambda: 'GET'
-    response = urllib.request.urlopen(req)
-    print(response.getcode())
-    print(response.read())
-except Exception as e:
-    print(e)
-####################################
