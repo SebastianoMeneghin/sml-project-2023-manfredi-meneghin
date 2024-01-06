60cbc3e43af6c9808a33562040f6040152f53af8
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2024-01-06 12:34:31 +0100

    Small correction in time-zone shifter

--- src/smhiAPI_mesan_timezone_shifter.py
+++ src/smhiAPI_mesan_timezone_shifter.py
@@ -14,7 +14,7 @@ from datetime import datetime
 ######################################################################################################
 
 # Read a checkpoint or a file (.csv) and load it on a dataframe
-file_name = 'smhiAPI_mesan_historical_data.csv'
+file_name = 'historical_data_from_checkpoint.csv'
 file_path = '/mnt/c/Developer/University/SML/sml-project-2023-manfredi-meneghin/datasets/smhi_historical_data/'
 complete_name = file_path + file_name
 df = pd.read_csv(complete_name)
@@ -31,26 +31,31 @@ for row in range(df.shape[0]):
     dd   = date_object.day
     hh   = df.at[row, 'time']
 
-    # Set when the DST was active (this refers to Sweden 2023, from 26th March - 01:00 (UCT00:00) to 29th October - 01:00 (UCT00:00))
-    if (mm == 3 and dd == 26 and hh >= 1) or (mm == 3 and dd >= 27) or (mm in {4,5,6,7,8,9}) or (mm == 10 and dd <= 28) or (mm == 10 and dd == 29 and hh == 0):
-        yyyy,mm,dd,hh = one_hour_forward(yyyy,mm,dd,hh)
-
     # Set when the DST goes from summer time to winter time, since one our computed is lost (In Sweden 2023: 29th Oct - 01:00 (UCT00:00))
     if (mm == 10 and dd == 29 and hh == 1):
+        print(row, mm, dd, hh)
         row_to_drop.append(row)
-        break
 
-    # According to the time-zone differences, the clock is moved forward or backward (In Sweden 2023: +01:00)
-    yyyy,mm,dd,hh = one_hour_forward(yyyy,mm,dd,hh)
+    else:
+        # Set when the DST was active (this refers to Sweden 2023, from 26th March - 01:00 (UCT00:00) to 29th October - 01:00 (UCT00:00))
+        if (mm == 3 and dd == 26 and hh >= 1) or (mm == 3 and dd >= 27) or (mm in {4,5,6,7,8,9}) or (mm == 10 and dd <= 28) or (mm == 10 and dd == 29 and hh == 0):
+            yyyy,mm,dd,hh = one_hour_forward(yyyy,mm,dd,hh)
 
-    # The old values are then replace by the just shifted date and time
-    df.at[row, 'date'] = get_date_label(yyyy, mm, dd, 'hyphen')
-    df.at[row, 'time'] = hh
+        # According to the time-zone differences, the clock is moved forward or backward (In Sweden 2023: +01:00)
+        print(row)
+        yyyy,mm,dd,hh = one_hour_forward(yyyy,mm,dd,hh)
+
+        # The old values are then replace by the just shifted date and time
+        df.at[row, 'date'] = get_date_label(yyyy, mm, dd, 'hyphen')
+        df.at[row, 'time'] = hh
+
 
 # Drop the DST deactivation's hour
 print(row_to_drop)
-df.drop(row_to_drop, inplace= True)
-
+print(df.loc[7080:7090])
+#print(df.at[row_to_drop[0],'date'], df.at[row_to_drop[0],'time'])
+df.drop(row_to_drop[0], inplace= True)
+print(df.loc[7080:7090])
 
 # Save the new dataframe in a new file (.csv)
 # Save clean dataframe in a new file (.csv)
