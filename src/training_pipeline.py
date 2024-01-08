7f45e057707caa0b0656530be11a5312dfcf1f4a
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2024-01-08 12:22:40 +0100

    Added hopsworks data retrieval in EDA and training pipeline

--- src/training_pipeline.py
+++ src/training_pipeline.py
@@ -9,41 +9,48 @@ import pandasql
 import pandas as pd
 import numpy as np
 import xgboost
-import sklearn
+import hopsworks
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import classification_report, confusion_matrix, mean_absolute_error, mean_squared_error
-from sklearn import metrics
-from datetime import datetime
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 # Select whether or not to select the best model and/or to evaluate them.
 model_selection  = False
 model_evaluation = False
 
+
 # Load dataset
-df = pd.read_csv('/mnt/c/Developer/University/SML/sml-project-2023-manfredi-meneghin/datasets/join_dataset_smhi_zyla.csv')
+hopsworks_api_key = os.environ['HOPSWORKS_API_KEY']
+project = hopsworks.login(api_key_value = hopsworks_api_key)
+fs = project.get_feature_store()
+
+fg = fs.get_feature_group(
+        name="flight_weather",
+        version=1,
+    )
+df = fg.read(dataframe_type = 'pandas')
 
 
 # Due to a not optimal flight API, some data as "DepApGate", "TimeTrip" cannot be calculated in new data
 # Furthermore, the following columns are dropped:
-df.drop(columns={'trip_time', 'depApGate'}, inplace = True)
+df.drop(columns={'trip_time', 'dep_ap_gate'}, inplace = True)
 
 # Due to a disproportion between categories and flights features (too mant for too few), the future columns are dropped:
 # When there will be more data, it will be interesting to add them to the model
-df.drop(columns={'airlineIataCode', 'flightIataNumber', 'arrApIataCode'}, inplace = True)
+df.drop(columns={'airline_iata_code', 'flight_iata_number', 'arr_ap_iata_code'}, inplace = True)
 
 # Some data are used as a key, but are not made to be variables of our model, furthermore are dropped:
 # If there will be data coming from different airports, it will be interesting to add 'depApIataCode' as variable.
-df.drop(columns={'status','depApIataCode', 'date'}, inplace = True)
+df.drop(columns={'status','dep_ap_iata_code', 'date'}, inplace = True)
 
 # Some data should be casted to int64
 convert_column = ['pressure','total_cloud', 'high_cloud', 'medium_cloud', 'low_cloud', 'sort_prep','humidity']
 for col in convert_column:
     df = df.astype({col: 'int64'})
 
-# Remove outliners in delay (depDelay > 120)
+# Remove outliners in delay (dep_delay > 120)
 row_list = []
 for row in range(df.shape[0]):
-  if (df.at[row, 'depDelay'] > 120):
+  if (df.at[row, 'dep_delay'] > 120):
     row_list.append(row)
 df.drop(row_list, inplace = True)
 df.reset_index(inplace = True)
@@ -69,10 +76,10 @@ df['wind_dir'] = direction_list
 # Instanciate a new model, create test set and dataset
 model = xgboost.XGBRegressor(eta= 0.1, max_depth= 7, n_estimators= 38, subsample= 0.8)
 train, test = train_test_split(df, test_size=0.2)
-Xtrain = train.drop(columns={'depDelay'})
-ytrain = train['depDelay']
-Xtest  = test.drop(columns={'depDelay'})
-ytest  = test['depDelay']
+Xtrain = train.drop(columns={'dep_delay'})
+ytrain = train['dep_delay']
+Xtest  = test.drop(columns={'dep_delay'})
+ytest  = test['dep_delay']
 
 # Train and test the model
 model.fit(Xtrain, ytrain)
@@ -88,10 +95,10 @@ if (model_selection):
 
     if (model_evaluation):
         train, eval = train_test_split(train, test_size = 0.125)
-        Xtrain = train.drop(columns={'depDelay'})
-        ytrain = train['depDelay']
-        Xeval  = eval.drop(columns={'depDelay'})
-        yeval  = eval['depDelay']
+        Xtrain = train.drop(columns={'dep_delay'})
+        ytrain = train['dep_delay']
+        Xeval  = eval.drop(columns={'dep_delay'})
+        yeval  = eval['dep_delay']
 
         eval_set = [(Xeval, yeval)]
 
