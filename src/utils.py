6b33c2919c0ea3966d4f2b8765a446660049ee07
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2024-01-08 22:40:11 +0100

    Added utility functions

--- src/utils.py
+++ src/utils.py
@@ -610,3 +610,53 @@ def get_today_date():
 
     return year, month, day
 
+
+def set_model_last_version_number(project, last_version_number):
+    '''
+    Given the Hopsworks Project "project", set the latest version number of the dataset to "version_number"
+    '''
+    # Get dataset API to save the version number on Hopsworks
+    dataset_api = project.get_dataset_api()
+
+    # Create the skeleton of a .json file big enough to be saved by Hopsworks
+    last_version_number = {'last_version_number': last_version_number}
+    version_number_list   = [last_version_number] * 1000
+    last_version_number_json = json.dumps(version_number_list)
+
+    # Create a file .json, save it in local and then save it on hopsworks. When finished, delete the json file locally
+    with open("last_version_number.json", "w") as outfile:
+        outfile.write(last_version_number_json)
+
+        last_version_number_path = os.path.abspath('last_version_number.json')
+        dataset_api.upload(last_version_number_path, "Resources/dataset_version", overwrite=True)
+    os.remove("last_version_number.json")
+    
+    
+def get_model_last_version_number(project):
+    '''
+    Given the Hopsworks Project "project", get the last_version_number of the dataset
+    '''
+    # Get dataset API to download the last_version_number from Hopsworks
+    dataset_api = project.get_dataset_api()
+    dataset_api.download("Resources/dataset_version/last_version_number.json")
+
+    # Open JSON file, return it as a dictionary
+    json_file = open('last_version_number.json')
+    json_data = json.load(json_file)
+
+    last_version_number = json_data[0]['last_version_number']
+
+    return last_version_number
+
+
+def dataset_normalizer(dataset_df):
+    '''
+    Given a dataset with the columns names extracted from the APIs data, return a dataset (dataframe)
+    with the name of the columns according to the feature group on Hopsworks
+    '''
+    dataset_df.rename(columns={'depApIataCode' : 'dep_ap_iata_code', 'depDelay' : 'dep_delay', 'depApTerminal': 'dep_terminal',
+                                'depApGate': 'dep_ap_gate', 'arrApIataCode' : 'arr_ap_iata_code', 'airlineIataCode':'airline_iata_code',
+                                'flightIataNumber':'flight_iata_number'}, inplace= True)
+    
+    return dataset_df
+
